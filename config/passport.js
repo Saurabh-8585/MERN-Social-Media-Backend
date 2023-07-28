f473a0e0300e152210b687139a490a4903906a14
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-07-28 14:24:18 +0530

    reset changes

--- config/passport.js
+++ config/passport.js
@@ -34,59 +34,56 @@ passport.use(
             callbackURL: '/api/auth/google/callback',
             scope: ['profile', 'email'],
         },
-        function (accessToken, refreshToken, profile, callback) {
-            callback(null, profile);
+        async function (accessToken, refreshToken, profile, done) {
+            try {
+                const existingUser = await User.findOne({ email: profile.emails[0].value });
+                if (existingUser) {
+                    const userToken = generateToken(existingUser);
+                    return done(null, { message: `Welcome back ${existingUser.username}`, token: userToken });
+                }
+
+                let tempPassword = generateRandomPassword();
+                const hashedPassword = await bcrypt.hash(tempPassword, 10);
+
+                const newUser = new User({
+                    username: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: hashedPassword,
+                });
+
+                await newUser.save();
+                const token = generateToken(newUser);
+
+                const response = {
+                    body: {
+                        name: profile.displayName,
+                        intro: 'Welcome to Snapia!',
+                        outro: [
+                            'You are receiving this email because you logged in with your Google account. As this is your first login, we have generated a temporary password for you',
+
+                            `<b>Temporary Password </b> : ${tempPassword}`,
+
+                            'For security purposes, we recommend that you change this temporary password at your earliest convenience. You can do so by visiting your account settings on Snapia\'s platform.',
+
+                            'If you have any questions or need further assistance, feel free to reach out to our support team. Welcome to Snapia, and we look forward to providing you with a seamless experience!',
+
+                        ],
+
+                        signature: 'Best regards,<br>Snapia Team'
+                    }
+                };
+
+                await generateMail({
+                    emailBody: response,
+                    to: profile.emails[0].value,
+                    subject: 'Welcome to Snapia!'
+                });
+                return done(null, { message: `Welcome ${newUser.username} ,Please check mail`, token });
+            } catch (error) {
+
+                return done(error, false);
+            }
         }
-        // async function (accessToken, refreshToken, profile, done) {
-        //     try {
-        //         const existingUser = await User.findOne({ email: profile.emails[0].value });
-        //         if (existingUser) {
-        //             const userToken = generateToken(existingUser);
-        //             return done(null, { message: `Welcome back ${existingUser.username}`, token: userToken });
-        //         }
-
-        //         let tempPassword = generateRandomPassword();
-        //         const hashedPassword = await bcrypt.hash(tempPassword, 10);
-
-        //         const newUser = new User({
-        //             username: profile.displayName,
-        //             email: profile.emails[0].value,
-        //             password: hashedPassword,
-        //         });
-
-        //         await newUser.save();
-        //         const token = generateToken(newUser);
-
-        //         const response = {
-        //             body: {
-        //                 name: profile.displayName,
-        //                 intro: 'Welcome to Snapia!',
-        //                 outro: [
-        //                     'You are receiving this email because you logged in with your Google account. As this is your first login, we have generated a temporary password for you',
-
-        //                     `<b>Temporary Password </b> : ${tempPassword}`,
-
-        //                     'For security purposes, we recommend that you change this temporary password at your earliest convenience. You can do so by visiting your account settings on Snapia\'s platform.',
-
-        //                     'If you have any questions or need further assistance, feel free to reach out to our support team. Welcome to Snapia, and we look forward to providing you with a seamless experience!',
-
-        //                 ],
-
-        //                 signature: 'Best regards,<br>Snapia Team'
-        //             }
-        //         };
-
-        //         await generateMail({
-        //             emailBody: response,
-        //             to: profile.emails[0].value,
-        //             subject: 'Welcome to Snapia!'
-        //         });
-        //         return done(null, { message: `Welcome ${newUser.username} ,Please check mail`, token });
-        //     } catch (error) {
-
-        //         return done(error, false);
-        //     }
-        // }
     )
 );
 

ca64c067e4241c705fde498ed2bcdabdc165c2fa
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-07-29 12:41:58 +0530

    removed scope

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,7 @@ passport.use(
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: 'http://snapia-backend.vercel.app/api/auth/google/callback',
-            scope: ['profile', 'email'],
+            // scope: ['profile', 'email'],
         },
         async function (accessToken, refreshToken, profile, done) {
             try {

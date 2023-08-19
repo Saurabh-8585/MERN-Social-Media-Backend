027c71e55b5f542ce6efd324fa81ae32ed5d6337
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-08-19 19:54:50 +0530

    removed passport from index

--- middleware/ApiAuth.js
+++ middleware/ApiAuth.js
@@ -9,7 +9,7 @@ function checkOrigin(req, res, next) {
     ];
 
     const origin = req.headers.origin;
-console.log({origin});
+    console.log({ origin });
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');

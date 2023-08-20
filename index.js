ad1411e90d96383c7fdb3432ddf86aac8b016c10
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-08-20 14:07:43 +0530

    changes in socket url

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'));
 app.use(helmet());
 app.use(cors({
-  // origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.BACKEND_URL],
-  origin: "*",
+  origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.BACKEND_URL],
+  // origin: "*",
   credentials: true
 }));
 
@@ -61,8 +61,7 @@ app.use('/api/message', message);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.BACKEND_URL],
-    origin: "*",
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.BACKEND_URL],
   },
 });
 

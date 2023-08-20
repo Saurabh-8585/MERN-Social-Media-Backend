de5cea11be496c5ba06c687ea0b7cfe569a0316c
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-08-20 13:16:02 +0530

    changes in socket url

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use('/api/message', message);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2],
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.BACKEND_URL],
   },
 });
 // socket-io for real time chat

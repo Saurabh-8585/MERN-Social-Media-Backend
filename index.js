56c23ee235a4fc4bbbb17c3dfc1a9d3dbbdc6173
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-08-24 15:53:43 +0530

    added new url

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use('/api/message', message);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.BACKEND_URL, 'wss://snapia-backend.vercel.app', 'https://snapia-backend.vercel.app'],
+    origin: ('*:*'),
     credentials: true
   },
 });
